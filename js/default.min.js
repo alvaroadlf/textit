8dc17e9644dd34fb1fbf88851f31c104bf58516c
Alvaro Araoz <alvaroaraoz_92@hotmail.com>
2017-11-15 11:41:53 +0100

    Add files via upload

---
+++ js/default.min.js
@@ -0,0 +1,18 @@
+var defaultTitle = 'Bienvenid@ a Text It';
+var defaultContent = 
+'<p>\
+A minimalist writing zone, where you can block out all distractions and get to what\'s important. The writing!  \
+</p>\
+<p> \
+	Para empezar lo unico que tienes que hacer es eliminar este texto (selecciona todo y elimina), cambia el título y empieza a escribir lo que quieras.\
+</p> \
+<p> \
+	Puedes usar <b>negrita</b>, <i>cursiva</i>, <b><i>ambas</i></b> y <a href="http://imalvaro.com/textit"> enlaces </a>, tan solo selecciona la(s) palabra(s) que quiera(s).\
+</p>\
+<blockquote>\
+	Las citas también son faciles de añadir\
+</blockquote>\
+<p>\
+	Si tienes <i>dudas</i> o quieres más información acerca del <b>código</b> haz clic en la pequeña interrogación que aparece abajo en la parte izquierda de la pantalla.\
+</p>\
+<p>O si quieres preguntame vía ~ <a href="mailto:alvaro.adlf@gmail.com">email</a>.</p>';
